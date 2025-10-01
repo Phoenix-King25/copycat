3a4484888e0292fb38f062da62aa8246d859d381
Phoenix-King25 <phoenixking025@gmail.com>
2025-10-01 23:16:30 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export default function handler(request, response) {
-  // This function runs on the server, so it can safely access process.env
-  const config = {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-  };
-
-  response.status(200).json(config);
-}
